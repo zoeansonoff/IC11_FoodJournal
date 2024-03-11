1d8e0276721760d7e0751f306ee070d5c6e85052
zoeansonoff <158520648+zoeansonoff@users.noreply.github.com>
2024-03-10 17:31:56 -0700

    Produce.java

--- src/Produce.java
+++ src/Produce.java
@@ -2,40 +2,31 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Produce extends PaleoFood implements Serializable {
-    private int Organic;
+    private int organic; // Change variable name to lowercase
 
-    public Produce(String name, int calories, int organic, int carbs) 
-    {
-        super(name, organic, 0);
-        this.calories = calories;
-        this.carbs = carbs;
-    }
-    public int getCalories() {
-        return calories;
-    }
-    public void setCalories(int calories) {
+    public Produce(String name, int calories, int organic, int carbs) {
+        super(name, calories, carbs); // Pass calories and carbs to the superclass constructor
         this.organic = organic;
     }
-    public int getCarbs() {
-        return carbs;
-    }
-    public void setCarbs(int carbs) {
-        this.carbs = carbs;
-    }
+
+    // Getters and setters for organic
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != 0.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Produce produce = (Produce) o;
         return organic == produce.organic;
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return Objects.hash(super.hashCode(), organic);
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         String output;
         if (organic == 1)
             output = "Organic: ";
@@ -45,4 +36,5 @@ public class Produce extends PaleoFood implements Serializable {
         return output;
     }
 }
+
 
